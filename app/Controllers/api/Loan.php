9f8d5f18748be42e3c9bf226598dff76925f5c7b
nuttapong <nuttaponmg.ep@gmail.com>
2024-12-06 15:08:21 +0700

    edit api

--- app/Controllers/api/Loan.php
+++ app/Controllers/api/Loan.php
@@ -27,6 +27,13 @@ class Loan extends BaseController
 
     public function ajaxDataTablePayment($id)
     {
+        header('Access-Control-Allow-Origin: http://localhost:8080');
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            exit; // Handle preflight request
+        }
 
         $response['code'] = 200;
         $response['message'] = '';
