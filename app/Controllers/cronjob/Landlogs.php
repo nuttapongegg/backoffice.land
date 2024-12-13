9fd259ca9cce75b2becf1b072c727c042f082fd3
nuttapong <nuttaponmg.ep@gmail.com>
2024-12-13 18:37:09 +0700

    edit logs

--- app/Controllers/cronjob/Landlogs.php
+++ app/Controllers/cronjob/Landlogs.php
@@ -17,12 +17,20 @@ class Landlogs extends BaseController
         $LandlogsModel = new LandlogsModel();
         $SettingLandModel = new SettingLandModel();
         try {
-            $loans = $LoanModel->getAllLoan();
+            $loans = $LoanModel->getAllDataLoan();
             $land_accounts = $SettingLandModel->getSettingLandAll();
 
+            $loan_payment_sum_installment = 0;
             $summary_no_vat_ON_STATE = 0;
             foreach ($loans as $loan) {
+
+                if ($loan->loan_payment_sum_installment != 0.00) {
+                    $loan_payment_sum_installment = $loan_payment_sum_installment + $loan->loan_payment_sum_installment;
+                }
+    
+                if ($loan->loan_status == 'ON_STATE') {
                     $summary_no_vat_ON_STATE = $summary_no_vat_ON_STATE + $loan->loan_summary_no_vat;
+                }
             }
             
             $sum_land_account = 0;
@@ -34,7 +42,7 @@ class Landlogs extends BaseController
             $LandlogsModel->insertLandlogs([
                 'land_logs_loan_amount' => $summary_no_vat_ON_STATE,
                 'land_logs_cash_flow' => $sum_land_account,
-                'land_logs_interest' => 0
+                'land_logs_interest' => $loan_payment_sum_installment
             ]);
 
         } catch (\Exception $e) {
